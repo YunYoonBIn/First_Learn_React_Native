da7ad2f3d88c83e4ccf1e0049b6967fbfa32277d
YunYoonBin <yubiny287@gmail.com>
2021-11-07 02:24:27 +0900

    RN

---
+++ 3장 컴포넌트/props/App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import MyButton from "./components/MyButton";
+
+export default function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Text style={{ fontSize: 30, marginBottom: 10 }}>props Btn</Text>
+
+ 
+
+      <MyButton title="Button" />
+
+      <StatusBar style="auto" />
+    </View>
+    );
+}
