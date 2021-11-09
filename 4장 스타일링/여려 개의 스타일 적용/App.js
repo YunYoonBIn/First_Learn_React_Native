de34943d6773826e5780dfbd9846dfac58e76dc4
YunYoonBin <yubiny287@gmail.com>
2021-11-09 20:58:50 +0900

    RN

---
+++ 4장 스타일링/여려 개의 스타일 적용/App.js
@@ -0,0 +1,35 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>
+       Class Styleng - text</Text>
+       <Text style={styles.text, styles.error}>
+       Class Styleng - error
+       </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    backgroundColor:"#fff",
+    alignItems:"center",
+    justifyContent: "center",
+  },
+  text: {
+    padding: 10,
+    fontSize: 26,
+    fontWeight: '600',
+    color: "black",
+  },
+  error: {
+    fontWeight: "400",
+    color: "red",
+  }
+})
+ 
